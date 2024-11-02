6b361d45f36d1596b480cd71f7b34856ddfef253
MyTruQs <raulfernandes.rm@gmail.com>
2024-11-02 10:58:58 -0300

    Estrutura da aplicação - Configurando temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -14,12 +14,17 @@ const buttonVariants = {
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100%;
+    width: 100px;
     height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
 
-    ${props => {
+    background-color: ${props => props.theme.primary}
+
+    /* ${props => {
         return css`
             background-color: ${buttonVariants[props.variant]};
         `
-    }}
+    }} */
 `
