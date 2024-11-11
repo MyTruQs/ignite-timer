81e32644a38e4e4535032b12dc2c66f6750d7fd9
MyTruQs <raulfernandes.rm@gmail.com>
2024-11-11 08:09:39 -0300

    Páginas e rotas - Finalização da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,14 +9,22 @@ export function Home() {
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
                         id="task"
+                        list="task-suggestions"
                         placeholder="Dê um nome para o seu projeto"
                     />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1"></option>
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesAmountInput
                         type="number"
                         id="minutesAmount"
                         placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
                     />
 
                     <span>minutos.</span>
