8a31e2bf7e104f9179e46a0a743c4fb04a1b7521
MyTruQs <raulfernandes.rm@gmail.com>
2024-11-15 23:17:55 -0300

    Funcionalidades da aplicação - Mudando title da página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -48,13 +48,20 @@ export function Home() {
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
     useEffect(() => {
+        let interval: ReturnType<typeof setInterval>;
+
         if (activeCycle) {
-            setInterval(() => {
+            interval = setInterval(() => {
                 setAmountSecondsPassed(
                     differenceInSeconds(new Date(), activeCycle.startDate)
                 )
             }, 1000);
         };
+
+        return () => {
+            clearInterval(interval);
+        }
+
     }, [activeCycle]);
 
     function handleCreateNewCycle(data: NewCycleFormData) {
@@ -67,9 +74,12 @@ export function Home() {
 
         setCycles((state) => [...state, newCycle]);
         setActiveCycleId(newCycle.id);
+        setAmountSecondsPassed(0);
 
         reset();
-    }
+    };
+
+
 
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
     const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
@@ -79,6 +89,12 @@ export function Home() {
 
     const minutes = String(minutesAmount).padStart(2, '0');
     const seconds = String(secondsAmount).padStart(2, '0');
+
+    useEffect(() => {
+        if (activeCycle) {
+            document.title = `${minutes}:${seconds}`
+        };
+    }, [minutes, seconds, activeCycle]);
 
     const task = watch('task');
     const isSubmitDisabled = !task;
